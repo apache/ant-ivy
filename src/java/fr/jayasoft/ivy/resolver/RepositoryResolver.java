3f4d0cc979f15fe649bf2bce4b8a22145a6818ab
Xavier Hanin <xavier@apache.org>
2006-01-18 18:08:30 +0000

    IMPROVE: possibility to configure ivy so that special revisions are not queried as fixed one at all (IVY-139)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/trunk@484108 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/fr/jayasoft/ivy/resolver/RepositoryResolver.java
+++ src/java/fr/jayasoft/ivy/resolver/RepositoryResolver.java
@@ -32,6 +32,7 @@ import fr.jayasoft.ivy.util.Message;
 public class RepositoryResolver extends AbstractResourceResolver {
     
     private Repository _repository;
+    private boolean _alwaysCheckExactRevision = true;
 
     
     public RepositoryResolver() {
@@ -54,51 +55,58 @@ public class RepositoryResolver extends AbstractResourceResolver {
 
 
     protected ResolvedResource findResourceUsingPattern(ModuleRevisionId mrid, String pattern, String artifact, String type, String ext, Date date) {
-        return findResourceUsingPattern(getName(), getRepository(), getLatestStrategy(), mrid, pattern, artifact, type, ext, date);
+        return findResourceUsingPattern(getName(), getRepository(), getLatestStrategy(), mrid, pattern, artifact, type, ext, date, isAlwaysCheckExactRevision());
     }
-    public static ResolvedResource findResourceUsingPattern(String name, Repository repository, LatestStrategy strategy, ModuleRevisionId mrid, String pattern, String artifact, String type, String ext, Date date) {
-        String resourceName = IvyPatternHelper.substitute(pattern, mrid, artifact, type, ext);
-        Message.debug("\t trying "+resourceName);
+    public static ResolvedResource findResourceUsingPattern(String name, Repository repository, LatestStrategy strategy, ModuleRevisionId mrid, String pattern, String artifact, String type, String ext, Date date, boolean alwaysCheckExactRevision) {
         try {
-            Resource res = repository.getResource(resourceName);
-            long start = System.currentTimeMillis();
-            boolean reachable = res.exists();
-            if (reachable) {
-                return new ResolvedResource(res, mrid.getRevision());
-            } else if (!mrid.isExactRevision()) {
-                ResolvedResource[] rress = ResolverHelper.findAll(repository, mrid, pattern, artifact, type, ext);
-                if (rress == null) {
-                    Message.debug("\t"+name+": unable to list resources for "+mrid+": pattern="+pattern);
-                    return null;
+            if (mrid.isExactRevision() || alwaysCheckExactRevision) {
+                String resourceName = IvyPatternHelper.substitute(pattern, mrid, artifact, type, ext);
+                Message.debug("\t trying "+resourceName);
+                Resource res = repository.getResource(resourceName);
+                long start = System.currentTimeMillis();
+                boolean reachable = res.exists();
+                if (reachable) {
+                    return new ResolvedResource(res, mrid.getRevision());
+                } else if (!mrid.isExactRevision()) {
+                    return findDynamicResourceUsingPattern(name, repository, strategy, mrid, pattern, artifact, type, ext, date);
                 } else {
-                    for (int i = 0; i < rress.length; i++) {
-                        Message.debug("\t"+name+": found "+rress[i]);
-                    }
-                }
-                ResolvedResource found;
-                if (rress.length == 1) {
-                    Message.debug("only one resource found: no need to use latest strategy");
-                     found = rress[0];
-                } else {
-                    found = (ResolvedResource)strategy.findLatest(rress, date);
-                }
-                if (found == null) {
-                    Message.debug("\t"+name+": no resource found for "+mrid+": pattern="+pattern);                    
-                } else if (!found.getResource().exists()) {
                     Message.debug("\t"+name+": resource not reachable for "+mrid+": res="+res);
-                    return null; 
+                    return null;
                 }
-                return found;
             } else {
-                Message.debug("\t"+name+": resource not reachable for "+mrid+": res="+res);
-                return null;
+                return findDynamicResourceUsingPattern(name, repository, strategy, mrid, pattern, artifact, type, ext, date);
             }
         } catch (Exception ex) {
-            Message.debug("\t"+name+": unable to get resource for "+mrid+": res="+resourceName+": "+ex.getMessage());
+            Message.debug("\t"+name+": unable to get resource for "+mrid+": res="+IvyPatternHelper.substitute(pattern, mrid, artifact, type, ext)+": "+ex.getMessage());
             return null;
         }
     }
-
+    
+    private static ResolvedResource findDynamicResourceUsingPattern(String name, Repository repository, LatestStrategy strategy, ModuleRevisionId mrid, String pattern, String artifact, String type, String ext, Date date) {
+        ResolvedResource[] rress = ResolverHelper.findAll(repository, mrid, pattern, artifact, type, ext);
+        if (rress == null) {
+            Message.debug("\t"+name+": unable to list resources for "+mrid+": pattern="+pattern);
+            return null;
+        } else {
+            for (int i = 0; i < rress.length; i++) {
+                Message.debug("\t"+name+": found "+rress[i]);
+            }
+        }
+        ResolvedResource found;
+        if (rress.length == 1) {
+            Message.debug("only one resource found: no need to use latest strategy");
+             found = rress[0];
+        } else {
+            found = (ResolvedResource)strategy.findLatest(rress, date);
+        }
+        if (found == null) {
+            Message.debug("\t"+name+": no resource found for "+mrid+": pattern="+pattern);                    
+        } else if (!found.getResource().exists()) {
+            Message.debug("\t"+name+": resource not reachable for "+mrid+": res="+found.getResource());
+            return null; 
+        }
+        return found;
+    }
     /**
      * Returns all resolved res matching the given pattern and matching given mrid, 
      * or null if no lister is able to handle the given pattern
@@ -159,5 +167,13 @@ public class RepositoryResolver extends AbstractResourceResolver {
     public void dumpConfig() {
         super.dumpConfig();
         Message.debug("\t\trepository: "+getRepository());
+    }
+
+    public boolean isAlwaysCheckExactRevision() {
+        return _alwaysCheckExactRevision;
+    }
+
+    public void setAlwaysCheckExactRevision(boolean alwaysCheckExactRevision) {
+        _alwaysCheckExactRevision = alwaysCheckExactRevision;
     }
 }
