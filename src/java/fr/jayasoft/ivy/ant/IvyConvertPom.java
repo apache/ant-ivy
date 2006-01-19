cdd27631230a1e1bd7c75c56fe12a07f9a62e666
Xavier Hanin <xavier@apache.org>
2006-01-19 09:56:42 +0000

    NEW: maven2 pom compatibility: most resolvers are now able to handle m2 pom as project metadata and there is a new convertpom task able to convert a pom file to an ivy file
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/trunk@484112 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/fr/jayasoft/ivy/ant/IvyConvertPom.java
@@ -0,0 +1,61 @@
+/*
+ * This file is subject to the license found in LICENCE.TXT in the root directory of the project.
+ * 
+ * #SNAPSHOT#
+ */
+package fr.jayasoft.ivy.ant;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.text.ParseException;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+
+import fr.jayasoft.ivy.Ivy;
+import fr.jayasoft.ivy.ModuleDescriptor;
+import fr.jayasoft.ivy.external.m2.PomModuleDescriptorParser;
+import fr.jayasoft.ivy.repository.url.URLResource;
+
+/**
+ * @author Hanin
+ *
+ */
+public class IvyConvertPom extends IvyTask {
+    private File _pomFile = null;
+    private File _ivyFile = null;
+    
+    public File getPomFile() {
+        return _pomFile;
+    }
+    public void setPomFile(File file) {
+        _pomFile = file;
+    }
+    public File getIvyFile() {
+        return _ivyFile;
+    }
+    public void setIvyFile(File ivyFile) {
+        _ivyFile = ivyFile;
+    }
+    
+    
+    public void execute() throws BuildException {
+        try {
+            if (_pomFile == null) {
+                throw new BuildException("source pom file is required for convertpom task");
+            }
+            if (_ivyFile == null) {
+                throw new BuildException("destination ivy file is required for convertpom task");
+            }
+            ModuleDescriptor md = PomModuleDescriptorParser.getInstance().parseDescriptor(new Ivy(), _pomFile.toURL(), false);
+            PomModuleDescriptorParser.getInstance().toIvyFile(_pomFile.toURL(), new URLResource(_pomFile.toURL()), getIvyFile(), md);
+        } catch (MalformedURLException e) {
+            throw new BuildException("unable to convert given pom file to url: "+_pomFile, e);
+        } catch (ParseException e) {
+            log(e.getMessage(), Project.MSG_ERR);
+            throw new BuildException("syntax errors in pom file "+_pomFile, e);
+        } catch (Exception e) {
+            throw new BuildException("impossible convert given pom file to ivy file: "+e.getMessage()+" from="+_pomFile+" to="+_ivyFile, e);
+        }
+    }
+}
---
+++ test/java/fr/jayasoft/ivy/ant/IvyConvertPomTest.java
@@ -0,0 +1,39 @@
+/*
+ * This file is subject to the licence found in LICENCE.TXT in the root directory of the project.
+ * Copyright Jayasoft 2005 - All rights reserved
+ * 
+ * #SNAPSHOT#
+ */
+package fr.jayasoft.ivy.ant;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import junit.framework.TestCase;
+
+import org.apache.tools.ant.Project;
+
+import fr.jayasoft.ivy.util.FileUtil;
+
+public class IvyConvertPomTest extends TestCase {
+    public void testSimple() throws Exception {
+        IvyConvertPom task = new IvyConvertPom();
+        task.setProject(new Project());
+        task.setPomFile(new File("test/java/fr/jayasoft/ivy/ant/test.pom"));
+        File destFile = File.createTempFile("ivy", ".xml");
+        destFile.deleteOnExit();
+        task.setIvyFile(destFile);
+        task.execute();
+        
+        String wrote = FileUtil.readEntirely(new BufferedReader(new FileReader(destFile)));
+        String expected = readEntirely("test-convertpom.xml");
+        assertEquals(expected, wrote);
+    }
+
+    private String readEntirely(String resource) throws IOException {
+        return FileUtil.readEntirely(new BufferedReader(new InputStreamReader(IvyConvertPomTest.class.getResource(resource).openStream()))).replaceAll("\r\n", System.getProperty("line.separator"));
+    }
+}
