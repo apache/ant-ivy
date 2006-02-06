040dbc4146523418709027a4b9b286c6586b48dc
Xavier Hanin <xavier@apache.org>
2006-02-06 14:55:27 +0000

    add httpclient test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/trunk@484197 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/java/fr/jayasoft/ivy/url/HttpclientURLHandlerTest.java
@@ -0,0 +1,21 @@
+/*
+ * This file is subject to the license found in LICENCE.TXT in the root directory of the project.
+ * 
+ * #SNAPSHOT#
+ */
+package fr.jayasoft.ivy.url;
+
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+/**
+ * Test HttpClientHandler
+ */
+public class HttpclientURLHandlerTest extends TestCase {
+    public void testIsReachable() throws Exception {
+        URLHandler handler = new HttpClientHandler();
+        assertTrue(handler.isReachable(new URL("http://www.google.fr/")));
+        assertFalse(handler.isReachable(new URL("http://www.google.fr/unknownpage.html")));
+    }
+}
