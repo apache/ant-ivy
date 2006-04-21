b8ddb3e9322c60ba0495c498f4b05bf0caabdabf
Xavier Hanin <xavier@apache.org>
2006-04-21 14:45:58 +0000

    NEW: A conflict manager that is able to allow implementation changes but not api changes (1.2.x - OK), (1.x.x - Not OK) (IVY-223) (thanks to Anders Janmyr)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/trunk@484290 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/fr/jayasoft/ivy/conflict/RegexpConflictManager.java
@@ -0,0 +1,118 @@
+/*
+ * This file is subject to the license found in LICENCE.TXT in the root directory of the project.
+ * 
+ * #SNAPSHOT#
+ */
+package fr.jayasoft.ivy.conflict;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import fr.jayasoft.ivy.IvyNode;
+import fr.jayasoft.ivy.util.Message;
+
+/**
+ * A ConflictManager that can be used to resolve conflicts based on regular
+ * expressions of the revision of the module. The conflict manager is added like
+ * this:
+ * 
+ * <pre>
+ *    &lt;typedef name=&quot;RegexpConflictManager&quot; classname=&quot;fr.jayasoft.ivy.conflict.RegexpConflictManager&quot; /&gt;
+ *    .
+ *    .
+ *    &lt;!-- Match all revisions, but ignore the last dot(.) and the character after it.
+ *        Used to match api changes in out milestones. --&gt;
+ *    &lt;conflict-managers&gt;
+ *        &lt;RegexpConflictManager name=&quot;regexp&quot; regexp=&quot;(.*)\..$&quot; ignoreNonMatching=&quot;true&quot;/&gt;
+ *    &lt;/conflict-managers&gt;
+ * </pre>
+ * 
+ * The regular expression must contain a capturing group. The group will be used
+ * to resolve the conflicts by an String.equals() test. If ignoreNonMatching is
+ * false non matching modules will result in an exception. If it is true they
+ * will be compaired by their full revision.
+ * 
+ * @author Anders janmyr
+ * 
+ */
+public class RegexpConflictManager extends AbstractConflictManager {
+	private Pattern pattern = Pattern.compile("(.*)");
+
+	private boolean mIgnoreNonMatching;
+
+	public RegexpConflictManager() {
+	}
+
+	public void setRegexp(String regexp) {
+		pattern = Pattern.compile(regexp);
+		Matcher matcher = pattern.matcher("abcdef");
+		if (matcher.groupCount() != 1) {
+			String message = "Pattern does not contain ONE (capturing group): '"
+					+ pattern + "'";
+			Message.error(message);
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public void setIgnoreNonMatching(boolean ignoreNonMatching) {
+		mIgnoreNonMatching = ignoreNonMatching;
+	}
+
+	public Collection resolveConflicts(IvyNode parent, Collection conflicts) {
+		IvyNode lastNode = null;
+		for (Iterator iter = conflicts.iterator(); iter.hasNext();) {
+			IvyNode node = (IvyNode) iter.next();
+
+			if (lastNode != null && !matchEquals(node, lastNode)) {
+				String msg = lastNode + ":" + getMatch(lastNode)
+						+ " (needed by " + lastNode.getParent()
+						+ ") conflicts with " + node + ":" + getMatch(node)
+						+ " (needed by " + node.getParent() + ")";
+				Message.error(msg);
+				Message.sumupProblems();
+				throw new StrictConflictException(msg);
+			}
+			if (lastNode == null || nodeIsGreater(node, lastNode)) {
+				lastNode = node;
+			}
+		}
+
+		return Collections.singleton(lastNode);
+	}
+
+	private boolean nodeIsGreater(IvyNode node, IvyNode lastNode) {
+		return getMatch(node).compareTo(getMatch(lastNode)) > 0;
+	}
+
+	private boolean matchEquals(IvyNode lastNode, IvyNode node) {
+		return getMatch(lastNode).equals(getMatch(node));
+	}
+
+	private String getMatch(IvyNode node) {
+		String revision = node.getId().getRevision();
+		Matcher matcher = pattern.matcher(revision);
+		if (matcher.matches()) {
+			String match = matcher.group(1);
+			if (match != null) {
+				return match;
+			}
+			warnOrThrow("First group of pattern: '" + pattern
+					+ "' does not match: " + revision + " " + node);
+		} else {
+			warnOrThrow("Pattern: '" + pattern + "' does not match: "
+					+ revision + " " + node);
+		}
+		return revision;
+	}
+
+	private void warnOrThrow(String message) {
+		if (mIgnoreNonMatching) {
+			Message.warn(message);
+		} else {
+			throw new StrictConflictException(message);
+		}
+	}
+}
---
+++ test/java/fr/jayasoft/ivy/conflict/RegexpConflictManagerTest.java
@@ -0,0 +1,59 @@
+/*
+ * This file is subject to the license found in LICENCE.TXT in the root directory of the project.
+ * 
+ * #SNAPSHOT#
+ */
+package fr.jayasoft.ivy.conflict;
+
+/**
+ * @author Anders janmyr
+ */
+
+import java.util.Date;
+
+import junit.framework.*;
+import fr.jayasoft.ivy.Ivy;
+
+
+public class RegexpConflictManagerTest extends TestCase
+{
+    private Ivy ivy;
+
+    protected void setUp() throws Exception
+    {
+        ivy = new Ivy();
+        ivy.configure( RegexpConflictManagerTest.class
+                .getResource( "ivyconf-regexp-test.xml" ) );
+    }
+
+    public void testNoApiConflictResolve() throws Exception
+    {
+        try
+        {
+            ivy.resolve( RegexpConflictManagerTest.class
+                    .getResource( "ivy-no-regexp-conflict.xml" ), null,
+                    new String[] { "*" }, null, new Date(), false );
+        }
+        catch ( StrictConflictException e )
+        {
+            fail( "Unexpected conflict: " + e );
+        }
+    }
+
+    public void testConflictResolve() throws Exception
+    {
+        try
+        {
+            ivy.resolve( RegexpConflictManagerTest.class
+                    .getResource( "ivy-conflict.xml" ), null,
+                    new String[] { "*" }, null, new Date(), false );
+
+            fail( "Resolve should have failed with a conflict" );
+        }
+        catch ( StrictConflictException e )
+        {
+            // this is expected
+            assertTrue(e.getMessage().indexOf("[ org1 | mod1.2 | 2.0.0 ]:2.0 (needed by [ jayasoft | resolve-noconflict | 1.0 ]) conflicts with [ org1 | mod1.2 | 2.1.0 ]:2.1 (needed by [ jayasoft | resolve-noconflict | 1.0 ])")!=-1);
+        }
+    }
+}
