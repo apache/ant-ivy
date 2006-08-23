817a9df7c4be8714a215ee315a7405e9787c29ce
Xavier Hanin <xavier@apache.org>
2006-08-23 17:04:16 +0000

    NEW: support for build number calculation (IVY-276)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/trunk@484471 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/fr/jayasoft/ivy/ant/IvyBuildNumber.java
@@ -0,0 +1,202 @@
+package fr.jayasoft.ivy.ant;
+
+import org.apache.tools.ant.BuildException;
+
+import fr.jayasoft.ivy.Ivy;
+import fr.jayasoft.ivy.ModuleId;
+import fr.jayasoft.ivy.ModuleRevisionId;
+import fr.jayasoft.ivy.ResolvedModuleRevision;
+
+/**
+ * Look for the latest module in the repository matching the given criteria, 
+ * and sets a set of properties according to what was found.
+ * 
+ * @author Xavier Hanin
+ */
+public class IvyBuildNumber extends IvyTask {
+	private String _organisation;
+	private String _module;
+	private String _branch;
+	private String _revision;
+	
+	private String _prefix = "ivy.";
+	private String _default = "0";
+	
+	public String getModule() {
+		return _module;
+	}
+
+	public void setModule(String module) {
+		_module = module;
+	}
+
+	public String getOrganisation() {
+		return _organisation;
+	}
+
+	public void setOrganisation(String organisation) {
+		_organisation = organisation;
+	}
+
+	public String getRevision() {
+		return _revision;
+	}
+
+	public void setRevision(String revision) {
+		_revision = revision;
+	}
+
+
+	public String getBranch() {
+		return _branch;
+	}
+
+	public void setBranch(String branch) {
+		_branch = branch;
+	}
+
+	public String getDefault() {
+		return _default;
+	}
+
+	public void setDefault(String default1) {
+		_default = default1;
+	}
+
+	public String getPrefix() {
+		return _prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		_prefix = prefix;
+	}
+
+	public void execute() throws BuildException {
+        if (_organisation == null) {
+            throw new BuildException("no organisation provided for ivy findmodules");
+        }
+        if (_module == null) {
+            throw new BuildException("no module name provided for ivy findmodules");
+        }
+        if (_prefix == null) {
+        	throw new BuildException("null prefix not allowed");
+        }
+        
+        Ivy ivy = getIvyInstance();
+        if (_branch == null) {
+            ivy.getDefaultBranch(new ModuleId(_organisation, _module));
+        }
+        if (_revision == null || _revision.length() == 0) {
+            _revision = "latest.integration";
+        } else if (!_revision.endsWith("+")) {
+        	_revision = _revision+"+";
+        }
+        if (!_prefix.endsWith(".") && _prefix.length() > 0) {
+        	_prefix = _prefix+".";
+        }
+		ResolvedModuleRevision rmr = ivy.findModule(ModuleRevisionId.newInstance(_organisation, _module, _branch, _revision));
+		String revision = rmr == null ? null : rmr.getId().getRevision();
+		NewRevision newRevision = computeNewRevision(revision);
+		setProperty("revision", newRevision.revision);
+		setProperty("new.revision", newRevision.newRevision);
+		setProperty("build.number", newRevision.buildNumber);
+		setProperty("new.build.number", newRevision.newBuildNumber);
+	}
+
+	private void setProperty(String propertyName, String value) {
+		if (value != null) {
+			getProject().setProperty(_prefix+propertyName, value);
+		}
+	}
+
+	private NewRevision computeNewRevision(String revision) {
+		if (revision == null) {
+			Range r = findLastNumber(_default);
+			if (r == null) { // no number found
+				return new NewRevision(revision, _default, null, null);
+			} else {
+				long n = Long.parseLong(_default.substring(r.startIndex, r.endIndex));
+				return new NewRevision(revision, _default, null, String.valueOf(n));
+			}
+		}
+		String revPrefix = "latest.integration".equals(_revision)?"":_revision.substring(0, _revision.length() - 1);
+		if (!revision.startsWith(revPrefix)) {
+			throw new BuildException("invalid exception found in repository: '"+revision+"' for '"+revPrefix+"'");
+		}
+		Range r;
+		if (revPrefix.length() == 0)  {
+			r = findLastNumber(revision);
+			if (r == null) {
+				 return new NewRevision(revision, revision+(revision.endsWith(".")?"1":".1"), null, "1");
+			}
+		} else {
+			r = findFirstNumber(revision, revPrefix.length());
+			if (r == null) {
+				 return new NewRevision(revision, revPrefix+(revPrefix.endsWith(".")?"1":".1"), null, "1");
+			}
+		}
+		long n = Long.parseLong(revision.substring(r.startIndex, r.endIndex)) + 1;
+		return new NewRevision(
+				revision, 
+				revision.substring(0, r.startIndex)+n, 
+				String.valueOf(n-1), 
+				String.valueOf(n));
+	}
+	
+	private Range findFirstNumber(String str, int startIndex) {
+		// let's find the first digit in the string
+		int startNumberIndex = startIndex;
+		while (startNumberIndex<str.length() && !Character.isDigit(str.charAt(startNumberIndex))) {
+			startNumberIndex++;
+		}
+		if (startNumberIndex == str.length()) {
+			return null;
+		}
+		// let's find the end of the number
+		int endNumberIndex = startNumberIndex+1;
+		while (endNumberIndex<str.length() && Character.isDigit(str.charAt(endNumberIndex))) {
+			endNumberIndex++;
+		}
+		return new Range(startNumberIndex, endNumberIndex);
+	}
+	
+	private Range findLastNumber(String str) {
+		int endNumberIndex = str.length() - 1;
+		while (endNumberIndex >= 0 && !Character.isDigit(str.charAt(endNumberIndex))) {
+			endNumberIndex--;
+		}
+		int startNumberIndex = endNumberIndex == -1?-1:endNumberIndex - 1;
+		while (startNumberIndex >= 0 && Character.isDigit(str.charAt(startNumberIndex))) {
+			startNumberIndex--;
+		}
+		endNumberIndex ++;
+		startNumberIndex ++;
+		if (startNumberIndex == endNumberIndex) { // no number found
+			return null;
+		} else {
+			return new Range(startNumberIndex, endNumberIndex);
+		}
+	}
+
+	private static class Range {
+		private int startIndex;
+		private int endIndex;
+		public Range(int startIndex, int endIndex) {
+			this.startIndex = startIndex;
+			this.endIndex = endIndex;
+		}
+	}
+
+	private static class NewRevision {
+		private String revision;
+		private String newRevision;
+		private String buildNumber;
+		private String newBuildNumber;
+		public NewRevision(String revision, String newRevision, String buildNumber, String newBuildNumber) {
+			this.revision = revision;
+			this.newRevision = newRevision;
+			this.buildNumber = buildNumber;
+			this.newBuildNumber = newBuildNumber;
+		}
+	}
+}
---
+++ test/java/fr/jayasoft/ivy/ant/IvyBuildNumberTest.java
@@ -0,0 +1,140 @@
+/*
+ * This file is subject to the license found in LICENCE.TXT in the root directory of the project.
+ * 
+ * #SNAPSHOT#
+ */
+package fr.jayasoft.ivy.ant;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Delete;
+
+public class IvyBuildNumberTest extends TestCase {
+    private File _cache;
+    private IvyBuildNumber _buildNumber;
+    
+    protected void setUp() throws Exception {
+        createCache();
+        Project project = new Project();
+        project.setProperty("ivy.conf.file", "test/repositories/ivyconf.xml");
+
+        _buildNumber = new IvyBuildNumber();
+        _buildNumber.setProject(project);
+    }
+
+    private void createCache() {
+        _cache = new File("build/cache");
+        _cache.mkdirs();
+    }
+    
+    protected void tearDown() throws Exception {
+        cleanCache();
+    }
+
+    private void cleanCache() {
+        Delete del = new Delete();
+        del.setProject(new Project());
+        del.setDir(_cache);
+        del.execute();
+    }
+
+    public void testDefault() throws Exception {
+    	_buildNumber.setOrganisation("org1");
+    	_buildNumber.setModule("newmod");
+    	_buildNumber.execute();
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.revision"));
+    	assertEquals("0", _buildNumber.getProject().getProperty("ivy.new.revision"));
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.build.number"));
+    	assertEquals("0", _buildNumber.getProject().getProperty("ivy.new.build.number"));
+    }
+    
+    public void testDefault2() throws Exception {
+    	_buildNumber.setOrganisation("org1");
+    	_buildNumber.setModule("newmod");
+    	_buildNumber.setDefault("1.0-dev-1");
+    	_buildNumber.execute();
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.revision"));
+    	assertEquals("1.0-dev-1", _buildNumber.getProject().getProperty("ivy.new.revision"));
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.build.number"));
+    	assertEquals("1", _buildNumber.getProject().getProperty("ivy.new.build.number"));
+    }
+    
+    public void testDefault3() throws Exception {
+    	_buildNumber.setOrganisation("org1");
+    	_buildNumber.setModule("newmod");
+    	_buildNumber.setDefault("mydefault");
+    	_buildNumber.execute();
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.revision"));
+    	assertEquals("mydefault", _buildNumber.getProject().getProperty("ivy.new.revision"));
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.build.number"));
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.new.build.number"));
+    }
+    
+    public void testLatest() throws Exception {
+    	_buildNumber.setOrganisation("org1");
+    	_buildNumber.setModule("mod1.1");
+    	_buildNumber.execute();
+    	assertEquals("2.0", _buildNumber.getProject().getProperty("ivy.revision"));
+    	assertEquals("2.1", _buildNumber.getProject().getProperty("ivy.new.revision"));
+    	assertEquals("0", _buildNumber.getProject().getProperty("ivy.build.number"));
+    	assertEquals("1", _buildNumber.getProject().getProperty("ivy.new.build.number"));
+    }
+    
+    public void testLatest2() throws Exception {
+    	_buildNumber.setOrganisation("orgbn");
+    	_buildNumber.setModule("buildnumber");
+    	_buildNumber.execute();
+    	assertEquals("test", _buildNumber.getProject().getProperty("ivy.revision"));
+    	assertEquals("test.1", _buildNumber.getProject().getProperty("ivy.new.revision"));
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.build.number"));
+    	assertEquals("1", _buildNumber.getProject().getProperty("ivy.new.build.number"));
+    }
+    
+    public void testPrefix() throws Exception {
+    	_buildNumber.setOrganisation("org1");
+    	_buildNumber.setModule("mod1.1");
+    	_buildNumber.setPrefix("test");
+    	_buildNumber.execute();
+    	assertEquals("2.0", _buildNumber.getProject().getProperty("test.revision"));
+    	assertEquals("2.1", _buildNumber.getProject().getProperty("test.new.revision"));
+    	assertEquals("0", _buildNumber.getProject().getProperty("test.build.number"));
+    	assertEquals("1", _buildNumber.getProject().getProperty("test.new.build.number"));
+    }
+    
+    public void testBuildNumber() throws Exception {
+    	_buildNumber.setOrganisation("org1");
+    	_buildNumber.setModule("mod1.1");
+    	_buildNumber.setRevision("1.");
+    	_buildNumber.execute();
+    	assertEquals("1.1", _buildNumber.getProject().getProperty("ivy.revision"));
+    	assertEquals("1.2", _buildNumber.getProject().getProperty("ivy.new.revision"));
+    	assertEquals("1", _buildNumber.getProject().getProperty("ivy.build.number"));
+    	assertEquals("2", _buildNumber.getProject().getProperty("ivy.new.build.number"));
+    }
+    
+    public void testBuildNumber2() throws Exception {
+    	_buildNumber.setOrganisation("org1");
+    	_buildNumber.setModule("mod1.5");
+    	_buildNumber.setRevision("1.");
+    	_buildNumber.execute();
+    	assertEquals("1.0.2", _buildNumber.getProject().getProperty("ivy.revision"));
+    	assertEquals("1.1", _buildNumber.getProject().getProperty("ivy.new.revision"));
+    	assertEquals("0", _buildNumber.getProject().getProperty("ivy.build.number"));
+    	assertEquals("1", _buildNumber.getProject().getProperty("ivy.new.build.number"));
+    }
+    
+    public void testBuildNumber3() throws Exception {
+    	_buildNumber.setOrganisation("org1");
+    	_buildNumber.setModule("mod1.1");
+    	_buildNumber.setRevision("1.1");
+    	_buildNumber.execute();
+    	assertEquals("1.1", _buildNumber.getProject().getProperty("ivy.revision"));
+    	assertEquals("1.1.1", _buildNumber.getProject().getProperty("ivy.new.revision"));
+    	assertEquals(null, _buildNumber.getProject().getProperty("ivy.build.number"));
+    	assertEquals("1", _buildNumber.getProject().getProperty("ivy.new.build.number"));
+    }
+    
+}
