e1cb8632efa23cf3925f171e22b9308708c3b564
Xavier Hanin <xavier@apache.org>
2007-01-02 20:25:18 +0000

    upgrade to new xooki version: fix bug with urls with no file name
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/trunk@491927 13f79535-47bb-0310-9956-ffa450edef68

--- src/doc/xooki/xooki/xooki.js
+++ src/doc/xooki/xooki/xooki.js
@@ -710,7 +710,13 @@ xooki.postProcess = function() {
         }
     })(xooki.toc, null, 0);
 	
-	var curPageId = new RegExp(".*\\/((?:.*\\/){"+xooki.config.level+"}[^\\/]*)(?:\\.\\w+)", "g").exec(window.location.toString())[1];
+	var match = new RegExp(".*\\/((?:.*\\/){"+xooki.config.level+"}[^\\/]*)(?:\\.\\w+)", "g").exec(window.location.toString());
+	var curPageId;
+	if (match == null || match[1] == '') {
+		curPageId = "index";
+	} else {
+		curPageId = match[1];
+	}
 	xooki.page = xooki.toc.pages[curPageId];
 
 	if (xooki.page == null) {
