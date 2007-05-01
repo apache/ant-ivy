4d99e66804d32c29e6928991fba9ca3661418d7d
Xavier Hanin <xavier@apache.org>
2007-05-01 15:58:35 +0000

    some ApacheCon presentation updates before the talk: - add demo 3 - slight review of slides - upgrade demos to 2.0 alpha1
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/core/trunk@534138 13f79535-47bb-0310-9956-ffa450edef68

---
+++ doc/presentations/apache-con-2007/demo/3/ivy-demo-3A/src/demo3A/Demo3A.java
@@ -0,0 +1,7 @@
+package demo3A;
+
+public class Demo3A {
+	public static String sayHello() {
+		return "Hello from Demo3A";
+	}
+}
---
+++ doc/presentations/apache-con-2007/demo/3/ivy-demo-3B/src/demo3B/Demo3B.java
@@ -0,0 +1,7 @@
+package demo3B;
+
+public class Demo3B {
+	public static String sayHello() {
+		return demo3A.Demo3A.sayHello();
+	}
+}
