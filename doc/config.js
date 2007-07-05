a8aeddc336b6753bae2b9696bc8eb97df0857e1d
Xavier Hanin <xavier@apache.org>
2007-07-05 13:36:54 +0000

    now use a config.js instead of config.json, and prepare import feature
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/core/trunk@553500 13f79535-47bb-0310-9956-ffa450edef68

--- doc/config.js
+++ doc/config.js
@@ -1,7 +1,7 @@
-{debug:true, 
+xooki.util.mix({debug:true, 
 	jira: {ids: ['IVY'], url: 'http://issues.apache.org/jira'}, 
 	shortcuts: {
 		svn: {pre: 'https://svn.apache.org/repos/asf/incubator/ivy/core/trunk/'},
-		ant: {pre: xooki.c.relativeRoot+'doc/use/', post:'.html'}
+		ant: {pre: xooki.c.relativeRoot+'use/', post:'.html'}
 	}
-}
+}, xooki.c, false);
