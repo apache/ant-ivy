f3f56d4f8fcae17bbf5090f296fb378b8b2f5485
Xavier Hanin <xavier@apache.org>
2007-09-26 05:33:51 +0000

    code cleanup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/core/trunk@579478 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/ivy/util/Message.java
+++ src/java/org/apache/ivy/util/Message.java
@@ -28,9 +28,14 @@ import java.util.Properties;
 import org.apache.ivy.core.IvyContext;
 
 /**
- * 
+ * Logging utility class.
+ * <p>
+ * To initialize Message you can call {@link #init(MessageImpl)} with
+ * the {@link MessageImpl} of your choice.
+ * <p> 
+ * This only takes effect in the current thread.
  */
-public class Message {
+public final class Message {
     // messages level copied from ant project, to avoid dependency on ant
     /** Message priority of "error". */
     public static final int MSG_ERR = 0;
@@ -47,15 +52,15 @@ public class Message {
     /** Message priority of "debug". */
     public static final int MSG_DEBUG = 4;
 
-    private static List _problems = new ArrayList();
+    private static List problems = new ArrayList();
 
-    private static List _warns = new ArrayList();
+    private static List warns = new ArrayList();
 
-    private static List _errors = new ArrayList();
+    private static List errors = new ArrayList();
 
-    private static boolean _showProgress = true;
+    private static boolean showProgress = true;
 
-    private static boolean _showedInfo = false;
+    private static boolean showedInfo = false;
 
     public static void init(MessageImpl impl) {
         IvyContext.getContext().setMessageImpl(impl);
@@ -80,7 +85,7 @@ public class Message {
     }
 
     private static void showInfo() {
-        if (!_showedInfo) {
+        if (!showedInfo) {
             Properties props = new Properties();
             URL moduleURL = Message.class.getResource("/module.properties");
             if (moduleURL != null) {
@@ -89,7 +94,7 @@ public class Message {
                     props.load(module);
                     debug("version information loaded from " + moduleURL);
                     info(":: Ivy " + props.getProperty("version") + " - "
-                            + props.getProperty("date") + " :: http://incubator.apache.org/ivy/ ::");
+                           + props.getProperty("date") + " :: http://incubator.apache.org/ivy/ ::");
                     module.close();
                 } catch (IOException e) {
                     info(":: Ivy non official version :: http://incubator.apache.org/ivy/ ::");
@@ -97,7 +102,7 @@ public class Message {
             } else {
                 info(":: Ivy non official version :: http://incubator.apache.org/ivy/ ::");
             }
-            _showedInfo = true;
+            showedInfo = true;
         }
     }
 
@@ -153,8 +158,8 @@ public class Message {
         } else {
             System.err.println(msg);
         }
-        _problems.add("WARN:  " + msg);
-        _warns.add(msg);
+        problems.add("WARN:  " + msg);
+        warns.add(msg);
     }
 
     public static void error(String msg) {
@@ -166,21 +171,21 @@ public class Message {
         } else {
             System.err.println(msg);
         }
-        _problems.add("\tERROR: " + msg);
-        _errors.add(msg);
+        problems.add("\tERROR: " + msg);
+        errors.add(msg);
     }
 
     public static List getProblems() {
-        return _problems;
+        return problems;
     }
 
     public static void sumupProblems() {
-        if (_problems.size() > 0) {
+        if (problems.size() > 0) {
             info("\n:: problems summary ::");
             MessageImpl messageImpl = IvyContext.getContext().getMessageImpl();
-            if (_warns.size() > 0) {
+            if (warns.size() > 0) {
                 info(":::: WARNINGS");
-                for (Iterator iter = _warns.iterator(); iter.hasNext();) {
+                for (Iterator iter = warns.iterator(); iter.hasNext();) {
                     String msg = (String) iter.next();
                     if (messageImpl != null) {
                         messageImpl.log("\t" + msg + "\n", MSG_WARN);
@@ -189,9 +194,9 @@ public class Message {
                     }
                 }
             }
-            if (_errors.size() > 0) {
+            if (errors.size() > 0) {
                 info(":::: ERRORS");
-                for (Iterator iter = _errors.iterator(); iter.hasNext();) {
+                for (Iterator iter = errors.iterator(); iter.hasNext();) {
                     String msg = (String) iter.next();
                     if (messageImpl != null) {
                         messageImpl.log("\t" + msg + "\n", MSG_ERR);
@@ -201,14 +206,14 @@ public class Message {
                 }
             }
             info("\n:: USE VERBOSE OR DEBUG MESSAGE LEVEL FOR MORE DETAILS");
-            _problems.clear();
-            _warns.clear();
-            _errors.clear();
+            problems.clear();
+            warns.clear();
+            errors.clear();
         }
     }
 
     public static void progress() {
-        if (_showProgress) {
+        if (showProgress) {
             MessageImpl messageImpl = IvyContext.getContext().getMessageImpl();
             if (messageImpl != null) {
                 messageImpl.progress();
@@ -223,7 +228,7 @@ public class Message {
     }
 
     public static void endProgress(String msg) {
-        if (_showProgress) {
+        if (showProgress) {
             MessageImpl messageImpl = IvyContext.getContext().getMessageImpl();
             if (messageImpl != null) {
                 messageImpl.endProgress(msg);
@@ -232,14 +237,17 @@ public class Message {
     }
 
     public static boolean isShowProgress() {
-        return _showProgress;
+        return showProgress;
     }
 
     public static void setShowProgress(boolean progress) {
-        _showProgress = progress;
+        showProgress = progress;
     }
 
     public static void uninit() {
         IvyContext.getContext().setMessageImpl(null);
+    }
+    
+    private Message() {
     }
 }
