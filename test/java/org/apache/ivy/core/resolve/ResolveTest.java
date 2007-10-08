7951705198f2fc2b204cc0044d5d6e9ed9ec3758
Scokart Gilles <gscokart@apache.org>
2007-10-08 12:53:11 +0000

    mod9.1 was already used by an other unit test using the repository 1
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/core/trunk@582809 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/ivy/core/resolve/ResolveTest.java
+++ test/java/org/apache/ivy/core/resolve/ResolveTest.java
@@ -1452,13 +1452,13 @@ public class ResolveTest extends TestCase {
     /**
      * Test IVY-618. 
      */
-/*    public void testResolveConflictFromPoms() throws Exception {
-        ResolveReport report = ivy.resolve(new File("test/repositories/2/mod9.1/ivy-1.0.xml")
+    public void testResolveConflictFromPoms() throws Exception {
+        ResolveReport report = ivy.resolve(new File("test/repositories/2/mod10.1/ivy-1.0.xml")
                 .toURL(), getResolveOptions(new String[] {"*"}));
         assertNotNull(report);
         assertFalse(report.hasError());
     }
-*/
+
     
     public void testTransitiveEviction() throws Exception {
         // mod7.3 depends on mod7.2 v1.0 and on mod7.1 v2.0
