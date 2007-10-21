1163a9a0481f34ad637b7879b9287feadb047f7a
Xavier Hanin <xavier@apache.org>
2007-10-21 08:41:51 +0000

    review and merge quick start and ivyrep tutorials, illustrating the use of maven 2 repository (IVY-591 and IVY-555)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ivy/core/trunk@586873 13f79535-47bb-0310-9956-ffa450edef68

--- src/example/hello-ivy/src/example/Hello.java
+++ src/example/hello-ivy/src/example/Hello.java
@@ -17,14 +17,30 @@
  */
 package example;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
 import org.apache.commons.lang.WordUtils;
 
 /**
- * Simple example world to show how easy it is to retreive libs with ivy !!! 
+ * Simple example to show how easy it is to retrieve transitive libs with ivy !!! 
  */
 public class Hello {
-    public static void main(String[] args) {
-        String  message = "hello ivy !";
+    public static void main(String[] args) throws Exception {
+        Option msg = OptionBuilder.withArgName( "msg" )
+        .hasArg()
+        .withDescription(  "the message to capitalize" )
+        .create( "message" );
+        Options options = new Options();
+        options.addOption(msg);
+        
+        CommandLineParser parser = new GnuParser();
+        CommandLine line = parser.parse( options, args );
+        
+        String  message = line.getOptionValue("message", "hello ivy !");
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() + " : " + WordUtils.capitalizeFully(message));
     }
