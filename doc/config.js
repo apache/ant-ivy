93ea897d6e9611970510f28215198f06b0354193
Xavier Hanin <xavier@apache.org>
2007-12-02 04:39:29 +0000

    relocate svn as a subproject of ant
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core/trunk@600235 13f79535-47bb-0310-9956-ffa450edef68

--- doc/config.js
+++ doc/config.js
@@ -1,7 +1,7 @@
 xooki.util.mix({debug:true, 
 	jira: {ids: ['IVY'], url: 'http://issues.apache.org/jira'}, 
 	shortcuts: {
-		svn: {pre: 'https://svn.apache.org/repos/asf/incubator/ivy/core/trunk/'},
+		svn: {pre: 'https://svn.apache.org/repos/asf/ant/ivy/core/trunk/'},
 		ant: {pre: xooki.c.relativeRoot+'use/', post:'.html'}
 	}
 }, xooki.c, false);
