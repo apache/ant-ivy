8af923c22d837f34834fd50d0b05eba8ed0d1a54
Maarten Coene <maartenc@apache.org>
2009-07-29 22:15:14 +0000

    FIX: URLResource does not properly support authentication (IVY-1106) (thanks to Heschi Kreinick)
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core/trunk@799097 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/ivy/plugins/repository/url/URLResource.java
+++ src/java/org/apache/ivy/plugins/repository/url/URLResource.java
@@ -96,6 +96,6 @@ public class URLResource implements Resource {
     }
 
     public InputStream openStream() throws IOException {
-        return url.openStream();
+        return URLHandlerRegistry.getDefault().openStream(url);
     }
 }
