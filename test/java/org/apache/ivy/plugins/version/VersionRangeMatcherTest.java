3197015c0d2efd8588faaf89e2ef30fba433dd56
Maarten Coene <maartenc@apache.org>
2010-05-31 19:48:33 +0000

    Improved code style (thanks to Carl Quinn)
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core/trunk@949849 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/ivy/plugins/version/VersionRangeMatcherTest.java
+++ test/java/org/apache/ivy/plugins/version/VersionRangeMatcherTest.java
@@ -23,24 +23,21 @@ import org.apache.ivy.core.module.id.ModuleRevisionId;
 import org.apache.ivy.plugins.latest.LatestRevisionStrategy;
 
 public class VersionRangeMatcherTest extends TestCase {
-    VersionMatcher _vm = new VersionRangeMatcher("range", new LatestRevisionStrategy());
+    VersionMatcher vm = new VersionRangeMatcher("range", new LatestRevisionStrategy());
 
-    public VersionRangeMatcherTest() {
-    }
-    
     public void testMavenExcludeParenthesis() throws Exception {
         assertAccept("[3.8,4.0)", "3.7", false);
         assertAccept("[3.8,4.0)", "3.8", true);
         assertAccept("[3.8,4.0)", "3.9", true);
         assertAccept("[3.8,4.0)", "4.0", false);
         assertAccept("[3.8,4.0)", "4.1", false);
-        
+
         assertAccept("(3.8,4.0]", "3.7", false);
         assertAccept("(3.8,4.0]", "3.8", false);
         assertAccept("(3.8,4.0]", "3.9", true);
         assertAccept("(3.8,4.0]", "4.0", true);
         assertAccept("(3.8,4.0]", "4.1", false);
-        
+
         assertAccept("(3.8,4.0)", "3.7", false);
         assertAccept("(3.8,4.0)", "3.8", false);
         assertAccept("(3.8,4.0)", "3.9", true);
@@ -131,7 +128,7 @@ public class VersionRangeMatcherTest extends TestCase {
         assertAccept("(,2.0]", "2.0", true);
 
         assertAccept("(,2.0]", "2.3", false);
-        
+
         assertAccept("[1.0, )", "1.1", true);
         assertAccept("[1.0 ,)", "2.0", true);
         assertAccept("[1.0 , )", "3.5.6", true);
@@ -157,11 +154,11 @@ public class VersionRangeMatcherTest extends TestCase {
     // assertion helper methods
 
     private void assertDynamic(String askedVersion, boolean b) {
-        assertEquals(b, _vm.isDynamic(ModuleRevisionId.newInstance("org", "name", askedVersion)));
+        assertEquals(b, vm.isDynamic(ModuleRevisionId.newInstance("org", "name", askedVersion)));
     }
 
     private void assertAccept(String askedVersion, String depVersion, boolean b) {
-        assertEquals(b, _vm.accept(ModuleRevisionId.newInstance("org", "name", askedVersion),
+        assertEquals(b, vm.accept(ModuleRevisionId.newInstance("org", "name", askedVersion),
             ModuleRevisionId.newInstance("org", "name", depVersion)));
     }
 }
