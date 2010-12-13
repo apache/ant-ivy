1959884c91908e144c06ec75e285a71c40f53d7d
Nicolas Lalevee <hibou@apache.org>
2010-12-13 23:18:59 +0000

    svn_apply_autoprops, mainly for line endings
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core/trunk@1045417 13f79535-47bb-0310-9956-ffa450edef68

--- doc/js/jquery.treeview.js
+++ doc/js/jquery.treeview.js
@@ -7,7 +7,7 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Revision: $Id: jquery.treeview.js 1443 2007-02-27 20:08:28Z joern $
+ * Revision: $Id$
  *
  */
 
