e0aedbbca453cb21d3fc847a36e498441d890f0d
Nicolas Lalevee <hibou@apache.org>
2011-04-28 21:51:30 +0000

    fix the tests regarding the previous commit r1097628
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core/trunk@1097631 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/ivy/osgi/updatesite/UpdateSiteLoaderTest.java
+++ test/java/org/apache/ivy/osgi/updatesite/UpdateSiteLoaderTest.java
@@ -17,26 +17,45 @@
  */
 package org.apache.ivy.osgi.updatesite;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Iterator;
 
 import junit.framework.TestCase;
 
+import org.apache.ivy.core.cache.CacheResourceOptions;
 import org.apache.ivy.core.module.descriptor.ModuleDescriptor;
+import org.apache.ivy.core.settings.IvySettings;
 import org.apache.ivy.osgi.repo.RepoDescriptor;
+import org.apache.ivy.util.CacheCleaner;
 import org.xml.sax.SAXException;
 
 public class UpdateSiteLoaderTest extends TestCase {
 
+    private UpdateSiteLoader loader;
+
+    private File cache;
+
+    protected void setUp() throws Exception {
+        IvySettings ivySettings = new IvySettings();
+        cache = new File("build/cache");
+        cache.mkdirs();
+        ivySettings.setDefaultCache(cache);
+        CacheResourceOptions options = new CacheResourceOptions();
+        loader = new UpdateSiteLoader(ivySettings.getDefaultRepositoryCacheManager(), null, options);
+    }
+
+    protected void tearDown() throws Exception {
+        CacheCleaner.deleteDir(cache);
+    }
+
     public void testIvyDE() throws IOException, ParseException, SAXException {
-        UpdateSiteLoader loader = new UpdateSiteLoader();
         RepoDescriptor site = loader.load("http://www.apache.org/dist/ant/ivyde/updatesite/");
         assertEquals(13, site.getModules().size());
     }
 
     public void testM2Eclipse() throws IOException, ParseException, SAXException {
-        UpdateSiteLoader loader = new UpdateSiteLoader();
         RepoDescriptor site = loader.load("http://m2eclipse.sonatype.org/sites/m2e/");
         assertTrue(site.getModules().size() > 70);
         Iterator itModules = site.getModules().iterator();
@@ -45,10 +64,12 @@ public class UpdateSiteLoaderTest extends TestCase {
             String name = md.getModuleRevisionId().getName();
             assertTrue(name, name.indexOf("org.maven") != -1);
         }
+
+        site = loader.load("http://m2eclipse.sonatype.org/sites/m2e/");
+        assertTrue(site.getModules().size() > 70);
     }
 
     public void _disabled_testHeliosEclipse() throws IOException, ParseException, SAXException {
-        UpdateSiteLoader loader = new UpdateSiteLoader();
         RepoDescriptor site = loader.load("http://download.eclipse.org/releases/helios/");
         assertTrue(site.getModules().size() > 900);
     }
