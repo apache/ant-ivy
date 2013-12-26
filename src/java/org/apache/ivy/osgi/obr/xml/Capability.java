6bce493e47fdac9e64a9ae1cb3c358f31975a43a
Nicolas Lalevee <hibou@apache.org>
2013-12-26 21:21:25 +0000

    add toString() methods for easier debug
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core/trunk@1553589 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/ivy/osgi/obr/xml/Capability.java
+++ src/java/org/apache/ivy/osgi/obr/xml/Capability.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 public class Capability {
 
-    private List/* <CapabilityProperty> */properties = new ArrayList/* <CapabilityProperty> */();
+    private List<CapabilityProperty> properties = new ArrayList<CapabilityProperty>();
 
     private String name;
 
@@ -38,7 +38,18 @@ public class Capability {
         properties.add(new CapabilityProperty(n, value, type));
     }
 
-    public List/* <CapabilityProperty> */getProperties() {
+    public List<CapabilityProperty> getProperties() {
         return properties;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append(name);
+        for (CapabilityProperty p : properties) {
+            buffer.append(" ");
+            buffer.append(p);
+        }
+        return buffer.toString();
     }
 }
--- src/java/org/apache/ivy/osgi/obr/xml/CapabilityProperty.java
+++ src/java/org/apache/ivy/osgi/obr/xml/CapabilityProperty.java
@@ -42,4 +42,10 @@ public class CapabilityProperty {
     public String getType() {
         return type;
     }
+
+    @Override
+    public String toString() {
+        return (type == null ? "" : "[" + type + "]") + name + "=" + value;
+    }
+
 }
