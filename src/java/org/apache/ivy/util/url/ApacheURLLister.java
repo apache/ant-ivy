902a680951f318488a4bbe0184a3a223c5709b01
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2014-10-28 00:36:39 +0100

    IVY-1452: NullPointerException when accessing charset to invalid URL (Thanks to Frédéric Riviere)

--- src/java/org/apache/ivy/util/url/ApacheURLLister.java
+++ src/java/org/apache/ivy/util/url/ApacheURLLister.java
@@ -29,6 +29,7 @@ import java.util.regex.Pattern;
 
 import org.apache.ivy.util.FileUtil;
 import org.apache.ivy.util.Message;
+import org.apache.ivy.util.url.URLHandler.URLInfo;
 
 /**
  * Utility class which helps to list urls under a given url. This has been tested with Apache 1.3.33
@@ -108,7 +109,13 @@ public class ApacheURLLister {
         }
 
         URLHandler urlHandler = URLHandlerRegistry.getDefault();
-        String charset = urlHandler.getURLInfo(url).getBodyCharset();
+        URLInfo urlInfo = urlHandler.getURLInfo(url);
+        if (urlInfo == URLHandler.UNAVAILABLE) {
+            return urlList; // not found => return empty list
+        }
+        // here, urlInfo is valid
+        String charset = urlInfo.getBodyCharset();
+
         InputStream contentStream = urlHandler.openStream(url);
         BufferedReader r = new BufferedReader(new InputStreamReader(contentStream, charset));
 
