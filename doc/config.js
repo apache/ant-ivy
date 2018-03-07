0c769150cc2be07dcafc74123cd51d23a7b58a2c
Jaikiran Pai <jaikiran@apache.org>
2018-03-07 15:52:25 +0530

    We now use asciidoc for documentation

--- doc/config.js
+++
@@ -1,8 +0,0 @@
-xooki.util.mix({debug:true, 
-	jira: {ids: ['IVY'], url: 'https://issues.apache.org/jira'}, 
-	shortcuts: {
-		gitdir: {pre: 'https://git-wip-us.apache.org/repos/asf?p=ant-ivy.git;a=tree;f='},
-		gitfile: {pre: 'https://git-wip-us.apache.org/repos/asf?p=ant-ivy.git;a=blob;f='},
-		ant: {pre: xooki.c.relativeRoot+'use/', post:'.html'}
-	}
-}, xooki.c, false);
