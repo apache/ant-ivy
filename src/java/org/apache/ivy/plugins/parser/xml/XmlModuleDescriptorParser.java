7b3ebeb083b3182075b2c046429c9376f6cd3651
Maarten Coene <4728619+maartenc@users.noreply.github.com>
2025-07-04 00:02:08 +0200

    Added missing '2.5' version and prepare for '2.6' as well

--- src/java/org/apache/ivy/plugins/parser/xml/XmlModuleDescriptorParser.java
+++ src/java/org/apache/ivy/plugins/parser/xml/XmlModuleDescriptorParser.java
@@ -219,7 +219,7 @@ public class XmlModuleDescriptorParser extends AbstractModuleDescriptorParser {
         }
 
         protected static final List<String> ALLOWED_VERSIONS = Arrays.asList("1.0",
-                "1.1", "1.2", "1.3", "1.4", "2.0", "2.1", "2.2", "2.3", "2.4");
+                "1.1", "1.2", "1.3", "1.4", "2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6");
 
         private static final String IVY_XSD_CONTENT;
 
