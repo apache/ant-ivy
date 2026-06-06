df64393433edab2ff6fb27435214ec7acad2a676
Stefan Bodewig <bodewig@apache.org>
2026-06-06 16:54:04 +0200

    skip tests if test server is not reachable

--- test/java/org/apache/ivy/util/url/BasicURLHandlerTest.java
+++ test/java/org/apache/ivy/util/url/BasicURLHandlerTest.java
@@ -28,6 +28,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
 /**
  * Test BasicURLHandler
@@ -67,6 +68,7 @@ public class BasicURLHandlerTest {
 
     @Test
     public void testContentEncoding() throws Exception {
+        assumeTrue(handler.isReachable(new URL("http://carsten.codimi.de/"), 5000));
         assertDownloadOK(new URL("http://carsten.codimi.de/gzip.yaws/daniels.html"), new File(
                 testDir, "gzip.txt"));
         assertDownloadOK(new URL(
